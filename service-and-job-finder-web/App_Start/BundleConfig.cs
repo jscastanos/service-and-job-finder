1287d733a52807083b635ef0aee76af07a884f4b
jscastanos <jec.castanos@gmail.com>
2020-02-11 10:28:02 +0800

    Change template from INSPINIA to ArchitectUI

--- service-and-job-finder-web/App_Start/BundleConfig.cs
+++ service-and-job-finder-web/App_Start/BundleConfig.cs
@@ -7,30 +7,20 @@ namespace service_and_job_finder_web
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-
-            ////////////////////////////////////////////// SCRIPTS ////////////////////////////////////////////
+          /////////////////////////////// SCRIPTS ////////////////////////////////////////////////////////
 
             bundles.Add(new ScriptBundle("~/Scripts/template").Include(
-                      "~/Scripts/template/jquery-2.1.1.js",
-                      "~/Scripts/template/bootstrap.min.js",
-                      "~/Scripts/template/plugins/metisMenu/jquery.metisMenu.js",
-                      "~/Scripts/template/plugins/slimscroll/jquery.slimscroll.min.js",
-                      "~/Scripts/template/inspinia.js",
-                      "~/Scripts/template/plugins/pace/pace.min.js"));
+                      "~/Scripts/architect-ui.js"));
 
             bundles.Add(new ScriptBundle("~/Scripts/angular").Include(
-                "~/Scripts/angular/angular.min.js",
-                "~/Scripts/angular/ng-app.js"
-                ));
+                 "~/Scripts/angular/angular.min.js",
+                 "~/Scripts/angular/ng-app.js"));
 
 
-                ////////////////////////////////////////// STYLES //////////////////////////////////////////////
+            /////////////////////////////// STYLES ////////////////////////////////////////////////////////
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.min.css",
-                      "~/fonts/font-awesome/css/font-awesome.min.css",
-                      "~/Content/animate.css",
-                      "~/Content/style.css"));
+                      "~/Content/architect-ui.css"));
         }
     }
 }
