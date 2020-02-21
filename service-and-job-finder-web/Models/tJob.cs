6d5d761e45497d27762c33abd14449ea59e872ee
jscastanos <jec.castanos@gmail.com>
2020-02-21 23:37:48 +0800

    removing EF5 libs, minor tweaks

--- service-and-job-finder-web/Models/tJob.cs
+++ service-and-job-finder-web/Models/tJob.cs
@@ -12,15 +12,12 @@ namespace service_and_job_finder_web.Models
     using System;
     using System.Collections.Generic;
     
-    public partial class tApplication
+    public partial class tJob
     {
         public int recNo { get; set; }
-        public string ApplicationId { get; set; }
-        public string PersonId { get; set; }
-        public string EntityId { get; set; }
-        public Nullable<System.DateTime> DateCreated { get; set; }
-        public Nullable<System.DateTime> DateUpdated { get; set; }
-        public Nullable<int> Status { get; set; }
         public string JobId { get; set; }
+        public string JobTitle { get; set; }
+        public string EntityId { get; set; }
+        public Nullable<int> Status { get; set; }
     }
 }
--- service-and-job-finder-web/Models/tServiceSet.cs
+++ service-and-job-finder-web/Models/tServiceSet.cs
@@ -12,22 +12,11 @@ namespace service_and_job_finder_web.Models
     using System;
     using System.Collections.Generic;
     
-    public partial class tPersonInfo
+    public partial class tServiceSet
     {
         public int recNo { get; set; }
-        public string PersonId { get; set; }
+        public string ServiceId { get; set; }
         public string UserId { get; set; }
-        public string Firstname { get; set; }
-        public string Middlename { get; set; }
-        public string Lastname { get; set; }
-        public Nullable<System.DateTime> Birthdate { get; set; }
-        public string Address { get; set; }
-        public string ContactNo { get; set; }
-        public string Title { get; set; }
-        public string Introduction { get; set; }
-        public byte[] ProfileImg { get; set; }
-        public Nullable<System.DateTime> DateCreated { get; set; }
-        public Nullable<System.DateTime> DateUpdated { get; set; }
         public Nullable<int> Status { get; set; }
     }
 }
